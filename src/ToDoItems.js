fd196c14638ed0d8a5edcbec3e54ed9898ecee84
Brad DeFauw <badefauw@gmail.com>
2022-05-24 10:07:37 -0500

    added remove elements to each todo item

--- src/ToDoItems.js
+++ src/ToDoItems.js
@@ -5,16 +5,20 @@ const item = (title, due) => {
     let check = document.createElement("div");
     check.classList.add("checkmark");
     todoItem.appendChild(check);
-
-    let titleText = document.createElement("p");
-    titleText.innerText = title;
-    todoItem.appendChild(titleText);
-
     //checkmark change
     check.addEventListener("click", function(){
         check.innerHTML = "<img src='images/check-bold.svg' />";
         check.style.background = 'linear-gradient(135deg, hsl(192, 100%, 67%), hsl(280, 87%, 65%))';
     })
+
+    let titleText = document.createElement("p");
+    titleText.innerText = title;
+    todoItem.appendChild(titleText);
+
+   let remove = document.createElement("p");
+   remove.innerText = "X";
+   remove.classList.add("remove");
+   todoItem.appendChild(remove);
 
     return todoItem;
 }
