bd4d849a8a8efe31d5c13733adf0024ccf638689
Brad DeFauw <badefauw@gmail.com>
2022-05-26 13:04:38 -0500

    get today's date and test if date value matches current date

--- src/newItem.js
+++ src/newItem.js
@@ -8,7 +8,16 @@ const newItem = () => {
         }else{
            console.log(todoInput.value); 
         }
-        console.log(date.value);
+        //get today's date
+        var today = new Date();
+        var dd = String(today.getDate()).padStart(2, '0');
+        var mm = String(today.getMonth() + 1).padStart(2, '0'); //January is 0!
+        var yyyy = today.getFullYear();
+
+        today = `${yyyy}-${mm}-${dd}`;
+        console.log(date.value, today);
+        //test if date value matches current date
+        console.log(date.value === today);
     })
 }
 
