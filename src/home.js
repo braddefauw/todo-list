adbe9bbffddd219d56ce641fdfc19b3bd15cff19
Brad DeFauw <badefauw@gmail.com>
2022-05-26 19:08:07 -0500

    home input background color bug fix

--- src/home.js
+++ src/home.js
@@ -8,6 +8,7 @@ const home = () => {
     homeBtn.addEventListener("click", function(){
         console.log(newTodo);
         newTodo.innerHTML = "";
+        newTodo.style.background = "rgba(77, 80, 102, 0.5)";
         main.innerHTML = "";
         todoInput();
         todoItems();
