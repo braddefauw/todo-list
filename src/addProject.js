c6888a1adbf8a9d731664ecd5ac5e990886a8db8
Brad DeFauw <badefauw@gmail.com>
2022-05-31 10:36:16 -0500

    add project popup invisible until add project button clicked on

--- src/addProject.js
+++ src/addProject.js
@@ -2,7 +2,15 @@ const addProject = () => {
     let projectBtn = document.querySelector(".add-proj");
     let newTodo = document.querySelector("#new-todo");
     let main = document.querySelector("#todo-list");
+
+    let newProjAdd = document.querySelector(".new-proj-add");
+    let addProjPopup = document.querySelector(".add-project-popup")
+    let addInput = document.querySelector(".add-proj-input")
+
     projectBtn.addEventListener("click", function(){
+        addProjPopup.style.display = "block";
+    })
+    newProjAdd.addEventListener("click", function(){
         newTodo.innerHTML = "NEW PROJECT";
         newTodo.style.fontSize = "2rem";
         main.innerHTML = "";
